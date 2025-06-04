663631860b07d287c9d6da23e2c4a5e926123494
mikel0x <mikel0x.dev@gmail.com>
2025-06-04 21:14:25 +0200

    Add project structure: logs, tools, writeups, notes

---
+++ hello_rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\mikel\rust-hacking\hello_rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+C:\Users\mikel\rust-hacking\hello_rust\target\debug\deps\hello_rust.d: src\main.rs
+
+src\main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+C:\Users\mikel\rust-hacking\hello_rust\target\debug\hello_rust.exe: C:\Users\mikel\rust-hacking\hello_rust\src\main.rs
